bc64f6c01e914a7267a74bb2a3fc1cefe62fe7a5
htask7878 <pdfile005@gmail.com>
2022-11-28 21:12:51 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ void main() async {
   await Firebase.initializeApp(
       // options: DefaultFirebaseOptions.currentPlatform,
       );
-  runApp(MaterialApp());
+  runApp(MaterialApp(
+    home: first(),
+  ));
 }
 
 class first extends StatefulWidget {
@@ -23,15 +25,16 @@ class first extends StatefulWidget {
 }
 
 class _firstState extends State<first> {
-  TextEditingController email=TextEditingController();
-  TextEditingController password=TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
 
   Future<UserCredential> signInWithGoogle() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
     // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
 
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
@@ -46,97 +49,96 @@ class _firstState extends State<first> {
   @override
   void initState() {
     super.initState();
-    final user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      print("This is not possible");
-    }else{
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-        return dashboard();
-      },));
-    }
-  }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final user = FirebaseAuth.instance.currentUser;
+      if (user == null) {
+        print("This is not possible");
+      } else {
+        print("This is  possible");
 
+        Navigator.pushReplacement(context, MaterialPageRoute(
+          builder: (context) {
+            return dashboard();
+          },
+        ));
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body:
-          Center(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding:
-                  const EdgeInsets.only(right: 18, left: 18, top: 15),
-                  child: TextField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: email,
-                    cursorColor: Color(0xff808080),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xff808080)),
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(15))),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xff808080)),
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(15))),
-                      contentPadding: EdgeInsets.all(10),
-                      hintText: "Email",
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding:
-                  const EdgeInsets.only(right: 18, left: 18, top: 15),
-                  child: TextField(
-                    obscureText: true,
-                    obscuringCharacter: "*",
-                    keyboardType: TextInputType.visiblePassword,
-                    controller: password,
-                    cursorColor: Color(0xff808080),
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xff808080)),
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(15))),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xff808080)),
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(15))),
-                      contentPadding: EdgeInsets.all(10),
-                      hintText: "Password",
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child:  GFButton(
-                    icon:Text("Sign in Google"),
-                    onPressed: (){
-                      signInWithGoogle().then((value) {
-                        print("");
-                        Navigator.push(context, MaterialPageRoute(builder: (context) {
-                          return dashboard();
-                        },));
-                      });
-                    },
-                  ),
-                ),
-              ],
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.only(right: 18, left: 18, top: 15),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                controller: email,
+                cursorColor: Color(0xff808080),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff808080)),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff808080)),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  contentPadding: EdgeInsets.all(10),
+                  hintText: "Email",
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 18, left: 18, top: 15),
+              child: TextField(
+                obscureText: true,
+                obscuringCharacter: "*",
+                keyboardType: TextInputType.visiblePassword,
+                controller: password,
+                cursorColor: Color(0xff808080),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff808080)),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xff808080)),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  contentPadding: EdgeInsets.all(10),
+                  hintText: "Password",
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton(
+                child: Text("Sign in Google"),
+                onPressed: () {
+                  signInWithGoogle().then((value) {
+                    print("hardik");
+                    Navigator.push(context, MaterialPageRoute(
+                      builder: (context) {
+                        return dashboard();
+                      },
+                    ));
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
 }
 /*todo
 // Name, email address, and profile photo URL
